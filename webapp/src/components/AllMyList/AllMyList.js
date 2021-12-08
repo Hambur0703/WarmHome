42748befa0041a49fe80ccff1e14ce811990870f
HanboHuang <huang.hanb@northeastern.edu>
2021-12-08 10:48:58 +0800

    web frame, login, reister

--- webapp/src/components/AllMyList/AllMyList.js
+++ webapp/src/components/AllMyList/AllMyList.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
-import { message } from 'antd';
+import {Image, message} from 'antd';
 import { isLogin } from "../../App";
+import axios from "axios";
 
 class AllMyList extends React.Component {
+    state ={
+        list:[]
+    }
+
+    getData = async()=>{
+        try{
+            const res =await axios.get('http://localhost:3002/apt/liked')
+            this.setState({list:res.data})
+            console.log('res',res)
+        }   catch {}
+    }
 
     componentDidMount() {
         if(!isLogin()){
             message.info('Please Login!')
+            return
         }
+        this.getData()
     }
 
     render() {
+        const {list} =this.state
         return !isLogin()?
             <Redirect to={'/login'} />  :(
-                <div>All my list</div>
+                <div>
+                    {
+                        list.map(v=><div key={v._id}>
+                            <Image
+                                width={200}
+                                src={v.aptImage}
+                            />
+                        </div>)
+                    }
+                </div>
             )
     }
 }
--- webapp/src/components/Main/Main.js
+++ webapp/src/components/Main/Main.js
@@ -1,22 +1,83 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
-import { message } from 'antd';
-import './Main.scss';
+import { message,Image } from 'antd';
 import { isLogin } from "../../App";
-
+import axios from "axios";
+import './Main.scss';
 
 class Main extends React.Component {
+    state ={
+        input:'',
+        list:[],
+        allList:[],
+        startPrice:'',
+        endPrice:''
+    }
+
+    getData = async()=>{
+        try{
+            const res =await axios.get('http://localhost:3002/apt')
+            this.setState({list:res.data,allList:res.data})
+            console.log('res',res)
+        }   catch {}
+    }
 
     componentDidMount() {
         if(!isLogin()){
             message.info('Please Login!')
+            return
         }
+        this.getData()
+    }
+
+    onSearch=()=>{
+        console.log('s',this.state.list)
+        const {input} =this.state
+        const list = this.state.list.filter(v=>v.aptAddress.includes(input))
+        this.setState({
+           list
+       })
+    }
+
+    onClear =()=>{
+        this.setState({list:this.state.allList,input:'',startPrice:'',endPrice:''})
+    }
+
+    onFilter=()=>{
+        const {startPrice,endPrice} =this.state
+        const list =[]
+        this.state.list.forEach(v=>{
+            if(v.aptPrice < Number(endPrice) && v.aptPrice> Number(startPrice)){
+                list.push(v)
+            }
+        })
+        this.setState({
+            list
+        })
     }
 
     render() {
+        const {list} =this.state
         return !isLogin()?
             <Redirect to={'/login'} />  :(
-          <div>main</div>
+          <div>
+              <input value={this.state.input} onChange={e=>this.setState({input:e.target.value})}/>
+              <button onClick={this.onSearch}>search</button>
+
+              <input onChange={e=>this.setState({startPrice:e.target.value})} placeholder='price'/>
+              <input onChange={e=>this.setState({endPrice:e.target.value})} placeholder='end price'/>
+              <button onClick={this.onFilter}>filter</button>
+
+              <button onClick={this.onClear}>clear</button>
+              {
+                  list.map(v=><div key={v._id}>
+                      <Image
+                          width={200}
+                          src={v.aptImage}
+                      />
+                  </div>)
+              }
+          </div>
         )
     }
 }
